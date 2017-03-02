66db29b9197eeccf9f9b4ff4e732a90bc9fa0bb5
Kevin <andersk82@yahoo.com>
2017-03-01 20:14:51 -0500

    axes now show up

--- models/Axes.js
+++ models/Axes.js
@@ -3,9 +3,9 @@ class Axes {
     let xcolor = vec3.fromValues(1, 71/255, 0.0);
     let ycolor = vec3.fromValues(0x38/255, 0x8e/255, 0x3c/255);
     let zcolor = vec3.fromValues(0x4f/255, 0xc3/255, 0xf7/255);
-    this.xaxis = new TruncCone(gl, 0.03, 0.03, 0.9, 20, xcolor, xcolor);
-    this.yaxis = new TruncCone(gl, 0.03, 0.03, 0.9, 20, ycolor, ycolor);
-    this.zaxis = new TruncCone(gl, 0.03, 0.03, 0.9, 20, zcolor, zcolor);
+    this.xaxis = new TruncCone(gl, 0.03, 0.03, 0.9, 20, 5, xcolor, xcolor);
+    this.yaxis = new TruncCone(gl, 0.03, 0.03, 0.9, 20, 5, ycolor, ycolor);
+    this.zaxis = new TruncCone(gl, 0.03, 0.03, 0.9, 20, 5, zcolor, zcolor);
     let rotypos90 = quat.setAxisAngle(quat.create(), vec3.fromValues(0,1,0), Math.PI/2);
     let rotxneg90 = quat.setAxisAngle(quat.create(), vec3.fromValues(1,0,0), -Math.PI/2);
     let transx = vec3.fromValues(0.45, 0, 0);
